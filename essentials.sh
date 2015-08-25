df395ceb5e51f44c3a37309c936751c979cb43a4
Jesse Cotton <jcotton1123@gmail.com>
2015-08-25 01:08:55 -0700

    essentials - use curl instead of wget

--- essentials.sh
+++ essentials.sh
@@ -21,7 +21,7 @@ fi
 # Key setup
 mkdir -p ~/.ssh
 chmod 0700 ~/.ssh
-wget -O ~/.ssh/authorized_keys --no-check-certificate https://github.com/jcotton1123.keys
+curl https://github.com/jcotton1123.keys >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
 # Bash profile
@@ -31,7 +31,7 @@ alias vi="vim" # Bad but w/e
 EOD
 
 # Setup Vim
-wget -O ~/.vimrc --no-check-certificate jcotton1123.github.io/bootstrapmeenv/configs/vimrc
+curl http://jcotton1123.github.io/boostrapmyenv/configs/vimrc > ~/.vimrc
 
 # Git configuration
 git config --global user.name "Jesse Cotton"
