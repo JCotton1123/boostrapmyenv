2aa82735c2023264c91f198aa641ff8ed2d5f69d
Jesse Cotton <jcotton1123@gmail.com>
2015-08-25 01:18:17 -0700

    LAMP - need to update ca-certs to install iUS repo

--- envs/rhel/lamp.sh
+++ envs/rhel/lamp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# Essential upgrades
+yum update -y ca-certificates
+
 # Repos
 yum -y install epel-release
 rpm -i https://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-14.ius.centos6.noarch.rpm
-
-# Essential upgrades
-yum update -y ca-certificates
 
 # Install php, apache, mysql
 yum install -y php55u php55u-mysql php55u-pdo php55u-intl php55u-mbstring
